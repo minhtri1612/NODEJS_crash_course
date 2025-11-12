067e5f1f4d2ec355c1700984a55cb1ebb9558c5e
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 01:24:32 +0700

    Blocking and Non-Blocking

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
@@ -20,15 +21,23 @@ const server = http.createServer((req, res) => {
         });
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            console.log(message);
+            let message = parsedBody.split('=')[1] || '';
+            message = decodeURIComponent(message.replace(/\+/g, ' '));
+            fs.writeFile('message.txt', message, err => {
+                if (err) {
+                    console.error('Failed to write message:', err);
+                    res.statusCode = 500;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Server error');
+                    return;
+                }
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
-
+    
 });
 
 server.listen(3000, () => {
