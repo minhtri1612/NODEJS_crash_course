bcd7507ca4f8f018567f0d4361de4741ba85319b
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-12 22:58:33 +0700

    redirecting requests

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
 
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && req.method === 'POST') {
+        fs.writeFileSync('message.txt', 'Dummy message');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
