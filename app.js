d1c2e144276c863790902a6e32ee573efde2af59
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 02:26:08 +0700

    middleware in nodejs and some changes when we use expressjs

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
 });
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
+// const server = http.createServer(routes);
+
+// server.listen(3000);
