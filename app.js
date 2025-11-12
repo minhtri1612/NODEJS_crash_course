f1614a980e83d6e86c13149359d96777e8758cf8
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 02:32:26 +0700

    Handling Different Routes

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
+    console.log('this always runs');
+    next();
+});
+
+app.use('/add-product',(req, res, next) => {
     console.log('In the middleware');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express!</h1>');
 });
