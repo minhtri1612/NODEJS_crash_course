3f68375913d418596b815677353e8c9d1f07e009
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 18:30:14 +0700

    fix

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/add-product',(req, res, next) => {
     console.log('In the middleware');
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-    next();
 });
 
 app.use('/product',(req, res, next) => {
