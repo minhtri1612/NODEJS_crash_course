f8cbf0ccb9b60979bc4f91d43bfde055ad7e2eec
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 20:58:28 +0700

    Serving Files Statically

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
