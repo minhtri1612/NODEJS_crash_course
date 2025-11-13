c475d6aaca3b7678c72a87b7218d953809b8acc0
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 20:17:00 +0700

    creating and serving HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 // /admin/add-product => GET
-router.use('/add-product',(req, res, next) => {
+router.get('/add-product',(req, res, next) => {
     console.log('In the middleware');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 // /admin/add-product => POST
 router.post('/add-product',(req, res, next) => {
