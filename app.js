dd254f77590665bf1a640431679d54c26bc7a4d6
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 18:23:53 +0700

    Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const http = require('http');
 const routes = require('./routes');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/',(req, res, next) => {
-    console.log('this always runs');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req, res, next) => {
     console.log('In the middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     next();
+});
+
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
