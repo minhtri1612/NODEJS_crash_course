110103fb6dea28a74f1dc54d1e17a60dba2105e3
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 22:55:00 +0700

    working with dynamic content & adding templating engines

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,22 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
+
+const express = require('express');
+
 const rootDir = require('../util/path');
+
+const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product',(req, res, next) => {
-    console.log('In the middleware');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
+
 // /admin/add-product => POST
-router.post('/add-product',(req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect('/');
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 exports.routes = router;
