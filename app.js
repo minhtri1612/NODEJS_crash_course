df90e6d3cb7be49c8ad9a725c24de6363a1fe736
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 20:25:50 +0700

    return 404 page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 const routes = require('./routes');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
 
@@ -14,7 +16,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
