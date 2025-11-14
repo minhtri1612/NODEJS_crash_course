601865a71bf6be79a4671d80438fbbb63848b47c
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-14 13:15:51 +0700

    Adding ProductID and extract Dynamic Patterns

--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,12 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/checkout', shopController.getCheckout);
+
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
