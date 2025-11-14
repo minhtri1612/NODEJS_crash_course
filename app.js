3d8b8bce9b0605c0f8190c704a6048dc86699642
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-14 09:14:17 +0700

    done MVC

--- app.js
+++ app.js
@@ -21,4 +21,30 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+// start server and attach error handlers so we don't get unhandled 'error' events
+const server = app.listen(3000, () => {
+	console.log('Server is listening on port 3000 (pid:', process.pid, ')');
+});
+
+server.on('error', (err) => {
+	console.error('Server error:', err && err.code ? err.code : err);
+	if (err && err.code === 'EADDRINUSE') {
+		console.error('Port 3000 is already in use. Please stop the other process or change the port.');
+		// exit with non-zero so nodemon knows it failed cleanly
+		process.exit(1);
+	} else {
+		// rethrow to allow default behavior for unexpected errors
+		throw err;
+	}
+});
+
+// Better global error visibility during development
+process.on('uncaughtException', (err) => {
+	console.error('Uncaught exception:', err);
+	process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+	console.error('Unhandled Rejection:', reason);
+	process.exit(1);
+});
--- models/product.js
+++ models/product.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(
+    
+    path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+const getProductFromFile = (cb) => {
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      else{
+        cb(JSON.parse(fileContent));
+      }
+    });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -7,34 +24,16 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+    getProductFromFile(products => {      
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
-      });
+        });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductFromFile(cb);
+
   }
 };
