1cd1ac1eed462b46f0aaa4cd581a728eb53f11da
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-14 12:15:54 +0700

    enhance the app

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
@@ -21,30 +21,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// start server and attach error handlers so we don't get unhandled 'error' events
-const server = app.listen(3000, () => {
-	console.log('Server is listening on port 3000 (pid:', process.pid, ')');
-});
-
-server.on('error', (err) => {
-	console.error('Server error:', err && err.code ? err.code : err);
-	if (err && err.code === 'EADDRINUSE') {
-		console.error('Port 3000 is already in use. Please stop the other process or change the port.');
-		// exit with non-zero so nodemon knows it failed cleanly
-		process.exit(1);
-	} else {
-		// rethrow to allow default behavior for unexpected errors
-		throw err;
-	}
-});
-
-// Better global error visibility during development
-process.on('uncaughtException', (err) => {
-	console.error('Uncaught exception:', err);
-	process.exit(1);
-});
-
-process.on('unhandledRejection', (reason) => {
-	console.error('Unhandled Rejection:', reason);
-	process.exit(1);
-});
+app.listen(3000);
--- models/product.js
+++ models/product.js
@@ -1,39 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const p = path.join(
-    
-    path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-const getProductFromFile = (cb) => {
 
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      else{
-        cb(JSON.parse(fileContent));
-      }
-    });
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductFromFile(products => {      
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
-        });
+      });
     });
   }
 
   static fetchAll(cb) {
-    getProductFromFile(cb);
-
+    getProductsFromFile(cb);
   }
 };
